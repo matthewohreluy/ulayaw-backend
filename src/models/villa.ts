681a7ad52c78fa78bb648281f5cfcf0628d18145
matthewohreluy <matthewohreluy@gmail.com>
2022-08-12 22:30:31 +0800

    changed villa schema

--- src/models/villa.ts
+++ src/models/villa.ts
@@ -1,54 +1,62 @@
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
+
+const villaDefinitionSchema = new Schema({
+   price:{
+      type: Number,
+      required: true
+     },
+     minPerson:{
+      type: Number,
+      required: true
+     },
+     maxPerson:{
+      type: Number,
+      required: true,
+     },
+     checkInTime:{
+      type: String,
+      required: true,
+     },
+     checkOutTime:{
+      type: String,
+      required: true,
+     },
+     inclusions:{
+      type: Array,
+      default:[]
+     },
+     ammenities:{
+      type: Array,
+      default:[]
+     },
+     addOns:{
+      type: Array,
+      default:[]
+     },
+     imageURLs:{
+      type: Array,
+      default:[]
+     },
+     note:{
+      type: String,
+      required: false
+     }
+});
+
 const villaSchema = new Schema({
    name:{
     type: String,
     required: true
    },
-   description:{
-    type: String,
-    required: false
+   overnight:{
+      type: villaDefinitionSchema,
+      required: true
    },
-   price:{
-    type: Number,
-    required: true
-   },
-   minPerson:{
-    type: Number,
-    required: true
-   },
-   maxPerson:{
-    type: Number,
-    required: true,
-   },
-   checkInTime:{
-    type: String,
-    required: true,
-   },
-   checkOutTime:{
-    type: String,
-    required: true,
-   },
-   inclusions:{
-    type: Array,
-    default:[]
-   },
-   ammenities:{
-    type: Array,
-    default:[]
-   },
-   addOns:{
-    type: Array,
-    default:[]
-   },
-   imageURLs:{
-    type: Array,
-    default:[]
-   },
-   note:{
-    type: String,
-    required: false
+   dayTour:{
+      type: villaDefinitionSchema,
+      required: true
    }
 });
 
