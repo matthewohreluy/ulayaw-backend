f7381f1e4a844c549ec0e4dd327a04d40bbc4f4b
matthewohreluy <matthewohreluy@gmail.com>
2022-09-10 20:09:32 +0800

    fixed cors

--- dist/middleware/util/cors.util.js
+++ dist/middleware/util/cors.util.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.cors = void 0;
 const cors = (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methids', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 };
--- src/middleware/util/cors.util.ts
+++ src/middleware/util/cors.util.ts
@@ -2,7 +2,7 @@ import {RequestHandler} from 'express';
 
 export const cors: RequestHandler = (req, res, next) =>{
     res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methids','GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 }
