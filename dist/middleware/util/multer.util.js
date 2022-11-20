8228e44c1aef6fa0203510be70e0ab6c5c6c7799
matthewohreluy <matthewohreluy@gmail.com>
2022-11-20 15:15:55 +0800

    done

--- dist/middleware/util/multer.util.js
+++ dist/middleware/util/multer.util.js
@@ -3,9 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.multer_image1 = exports.multer_moment = void 0;
+exports.multer = void 0;
 const multer_1 = __importDefault(require("multer"));
-const fileStorage_moments = multer_1.default.diskStorage({
+const fileStorage = multer_1.default.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'src/static/moments');
     },
@@ -13,17 +13,6 @@ const fileStorage_moments = multer_1.default.diskStorage({
         cb(null, new Date().getTime() + file.originalname);
     }
 });
-exports.multer_moment = (0, multer_1.default)({
-    storage: fileStorage_moments
-}).single('file');
-const fileStorage_image1 = multer_1.default.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'src/static/villa');
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().getTime() + file.originalname);
-    }
-});
-exports.multer_image1 = (0, multer_1.default)({
-    storage: fileStorage_moments
+exports.multer = (0, multer_1.default)({
+    storage: fileStorage
 }).single('file');
--- src/middleware/util/multer.util.ts
+++ src/middleware/util/multer.util.ts
@@ -1,5 +1,5 @@
 import Multer from 'multer'
-const fileStorage_moments = Multer.diskStorage({
+const fileStorage = Multer.diskStorage({
     destination: (req, file, cb)=>{
         cb(null, 'src/static/moments')
     },
@@ -7,19 +7,6 @@ const fileStorage_moments = Multer.diskStorage({
         cb(null, new Date().getTime() + file.originalname)
     }
 })
-export const multer_moment = Multer({
-    storage: fileStorage_moments
-}).single('file');
-
-const fileStorage_image1 = Multer.diskStorage({
-    destination: (req, file, cb)=>{
-        cb(null, 'src/static/villa')
-    },
-    filename: (req,file,cb)=>{
-        cb(null, new Date().getTime() + file.originalname)
-    }
-})
-
-export const multer_image1 = Multer({
-    storage: fileStorage_moments
+export const multer = Multer({
+    storage: fileStorage
 }).single('file');
